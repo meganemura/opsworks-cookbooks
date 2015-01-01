6a6a7f7c457b616144cf3dbd41870538e61c613b
meganemura <mura2megane@gmail.com>
2015-01-02 01:18:40 +0900

    my-sonar-runner: Add sonar-runner installation

--- my-sonar-runner/recipes/default.rb
+++ my-sonar-runner/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+sonar_runner_zipfile_destination = ::File.join(Chef::Config[:file_cache_path], "sonar-runner-dist-2.4.zip")
+
+remote_file sonar_runner_zipfile_destination do
+  source "http://repo1.maven.org/maven2/org/codehaus/sonar/runner/sonar-runner-dist/2.4/sonar-runner-dist-2.4.zip"
+  mode 644
+end
+
+package 'unzip'
+
+bash 'unzip installation' do
+  code <<-EOF
+    unzip #{sonar_runner_zipfile_destination} -d /opt/
+  EOF
+  not_if { ::File.directory?("/opt/sonar-runner-2.4/") }
+end
