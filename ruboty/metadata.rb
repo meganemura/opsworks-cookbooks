acca1253987a2f34aeeb7cc72b101486a967755f
meganemura <meganemura@users.noreply.github.com>
2015-03-12 01:45:50 +0900

    berks cookbook ruboty

---
+++ ruboty/metadata.rb
@@ -0,0 +1,7 @@
+name             'ruboty'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ruboty'
+long_description 'Installs/Configures ruboty'
+version          '0.1.0'
