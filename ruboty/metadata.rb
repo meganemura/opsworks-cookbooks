e1962a92c64e001bc4cd0374abd948b0e724eff6
meganemura <meganemura@users.noreply.github.com>
2015-03-12 01:59:23 +0900

    Add rbenv as dependency

--- ruboty/metadata.rb
+++ ruboty/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures ruboty'
 long_description 'Installs/Configures ruboty'
 version          '0.1.0'
+
+depends 'rbenv'
--- ruboty/recipes/default.rb
+++ ruboty/recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+
+rbenv_ruby "2.1.4"
+rbenv_gem "bundler" do
+  ruby_version "2.1.4"
+end
