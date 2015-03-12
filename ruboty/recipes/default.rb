d050b720ede5b8bbcf2695bab9c09ded837383cf
meganemura <meganemura@users.noreply.github.com>
2015-03-13 00:03:38 +0900

    ruboty: Configure rbenv

--- ruboty/recipes/default.rb
+++ ruboty/recipes/default.rb
@@ -13,3 +13,11 @@ rbenv_ruby "2.1.4"
 rbenv_gem "bundler" do
   ruby_version "2.1.4"
 end
+
+file "/home/ubuntu/.ruby-version" do
+  owner 'ubuntu'
+  group 'ubuntu'
+  mode '0755'
+  content '2.1.4'
+  action :create_if_missing
+end
